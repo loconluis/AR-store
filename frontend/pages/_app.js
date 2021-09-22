1aaf458677c6a4c76ecd329f55e1a4c99c17fbd8
Luis Locon <luis.locon@gmail.com>
2021-09-22 00:44:35 -0600

    Adding some global styles and route changes lib

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,7 +1,16 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
+import NProgress from 'nprogress';
+import Router from 'next/router';
 import Page from '../components/Page';
+// ⁄Todo swap with our own
+import 'nprogress/nprogress.css';
+import '../components/styles/nprogress.css';
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 const MyApp = ({ Component, pageProps }) => (
   <Page>
